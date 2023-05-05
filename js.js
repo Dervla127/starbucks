2285b2103b20945b1a73dd66c43adeef153b959b
Dervla127 <132310815+Dervla127@users.noreply.github.com>
2023-05-05 08:42:19 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+<script src="script.js">
+	  const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)</script>
